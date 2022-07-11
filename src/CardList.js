e5da4727e61fa4383bc27f9027527fea35f7c386
Colter Garrison <102547626+Colter-Garrison@users.noreply.github.com>
2022-07-11 14:41:55 -0700

    got cards showing

--- src/CardList.js
+++ src/CardList.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import Card from './Card';
+import { useCardContext } from './ContextProvider';
 
-export default function CardList({ cards }) {
+export default function CardList() {
+  const { deck, setSelectedCard, player, setFrom, selectedCard } = useCardContext();
   return (
     <div className='card-list'>
       {
-        cards.map((card => <Card 
+        deck.map((card => <Card 
           key={card.suit + card.value} 
-          // setSelectedCard={setSelectedCard} 
-          // selectedCard={selectedCard}
-          // player={player}
-          // setFrom={setFrom}
+          setSelectedCard={setSelectedCard} 
+          selectedCard={selectedCard}
+          player={player}
+          setFrom={setFrom}
           card={card}
         />))
       }
